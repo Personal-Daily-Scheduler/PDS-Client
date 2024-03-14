d669cb06f75480cfc302a8818f9d96966d832cbf
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-14 23:03:33 +0900

    ✨ [feature] 가입한 유저의 plan 목록 DB 요청 로직

---
+++ src/services/fetchGetPlans.js
@@ -0,0 +1,29 @@
+import generateApiUri from '../utils/generateURI';
+
+const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
+
+const fetchUserPlans = async (user) => {
+  const BASE_URI = generateApiUri(SERVER_URI, `users/${user.userId}/diaries/plans`);
+
+  try {
+    const response = await fetch(BASE_URI, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseJson = await response.json();
+
+    return responseJson;
+  } catch (error) {
+    console.error('Error fetching user plans:', error);
+  }
+};
+
+export default fetchUserPlans;
