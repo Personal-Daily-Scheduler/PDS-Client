994df2da9b94c824091095d7670765f2ca0cb521
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-14 23:04:48 +0900

    ✨ [feature] 가입한 유저의 plan DB 삭제 로직

---
+++ src/services/fetchRemovePlan.js
@@ -0,0 +1,26 @@
+import generateApiUri from '../utils/generateURI';
+
+const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
+
+const fetchRemovePlan = async (plansObject, memberUser) => {
+  const BASE_URI = generateApiUri(SERVER_URI, `users/${memberUser.userId}/diaries/${plansObject.selectedDate}/plan`);
+
+  try {
+    const response = await fetch(BASE_URI, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${memberUser.token}`,
+      },
+      body: JSON.stringify({ planId: plansObject.planId }),
+    });
+
+    if (!response.ok) {
+      console.error('Failed to delete plan from database');
+    }
+  } catch (error) {
+    console.error('Error deleting plan from database:', error);
+  }
+};
+
+export default fetchRemovePlan;
