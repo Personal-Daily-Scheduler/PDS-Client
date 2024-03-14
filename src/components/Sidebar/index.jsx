992587ce5315bc64a1334748e010b24e7f7808d8
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-14 23:24:15 +0900

    ✨ [feature] 좌측 사이드바 로그아웃 버튼 기능 추가
    로그아웃 클릭시 모든 전역상태 초기화 및 세션스토리지 삭제 후 초기페이지 리다이렉트

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
 import CustomCalendar from '../Calendar';
+
+import useUserStore from '../../store/user';
+import useCalendarStore from '../../store/calender';
+import usePlanStore from '../../store/plans';
 
 import calendarIcon from '../../assets/calendar_icon.png';
 import todayIcon from '../../assets/today_icon.png';
@@ -9,11 +15,21 @@ import profileIcon from '../../assets/profile_icon.png';
 import logoutIcon from '../../assets/logout_icon.png';
 
 function Sidebar({ isSidebarOpen, toggleSidebar }) {
-  const [value, onChange] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState(null);
+  const { username, clearUser } = useUserStore();
+  const { clearCalendar } = useCalendarStore();
+  const { clearPlan } = usePlanStore();
+
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    console.log('Request Logout');
+    sessionStorage.removeItem('authenticatedUser');
+    sessionStorage.removeItem('guestUser');
+
+    clearCalendar();
+    clearPlan();
+    clearUser();
+
+    navigate('/');
   };
 
   function IconButton(imageUri) {
@@ -34,33 +50,31 @@ function Sidebar({ isSidebarOpen, toggleSidebar }) {
       <ToggleButton isSidebarOpen={isSidebarOpen} onClick={toggleSidebar}>
         {isSidebarOpen ? '<' : '>'}
       </ToggleButton>
-      <div>
-        {isSidebarOpen && (
-          <>
-            <MenuWrapper>
-              {IconButton(profileIcon)}
-              <ProfileText>고경준 님</ProfileText>
-            </MenuWrapper>
-            <MenuWrapper>
-              {IconButton(addTaskIcon)}
-              <Button>Add Task</Button>
-            </MenuWrapper>
-            <MenuWrapper>
-              {IconButton(todayIcon)}
-              <Button>Today</Button>
-            </MenuWrapper>
-            <MenuWrapper>
-              {IconButton(calendarIcon)}
-              <Button>Calendar</Button>
-            </MenuWrapper>
-            <CustomCalendar />
-            <MenuWrapper onClick={handleLogout}>
-              {IconButton(logoutIcon)}
-              <LogoutButtonText>Logout</LogoutButtonText>
-            </MenuWrapper>
-          </>
-        )}
-      </div>
+      {isSidebarOpen && (
+        <>
+          <MenuWrapper>
+            {IconButton(profileIcon)}
+            <ProfileText>{username}</ProfileText>
+          </MenuWrapper>
+          <MenuWrapper>
+            {IconButton(addTaskIcon)}
+            <Button>Add Task</Button>
+          </MenuWrapper>
+          <MenuWrapper>
+            {IconButton(todayIcon)}
+            <Button>Today</Button>
+          </MenuWrapper>
+          <MenuWrapper>
+            {IconButton(calendarIcon)}
+            <Button>Calendar</Button>
+          </MenuWrapper>
+          <CustomCalendar />
+          <MenuWrapper onClick={handleLogout}>
+            {IconButton(logoutIcon)}
+            <LogoutButtonText>Logout</LogoutButtonText>
+          </MenuWrapper>
+        </>
+      )}
     </SidebarContainer>
   );
 }
