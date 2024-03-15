8813e759850a8a2e2f1ba20fb0160a21d0b34124
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-15 21:40:23 +0900

    ✨ [feature] 메인 페이지 라우트에서 Schedule 컴포넌트 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 import Login from './Login';
 import Layout from './Layout';
 import Plans from './Plans';
+import Schedules from './Schedules';
 
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Login />} />
       <Route element={<Layout />}>
-        <Route path="/users" exact element={<Plans />} />
+        <Route
+          path="/users"
+          exact
+          element={(
+            <Wrapper>
+              <Plans />
+              <Schedules />
+            </Wrapper>
+        )}
+        />
       </Route>
     </Routes>
   );
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export default App;
