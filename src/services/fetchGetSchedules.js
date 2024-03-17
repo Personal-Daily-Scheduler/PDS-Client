09ff5d09e7062bb9ad055c694367bef1e3a511f5
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-17 16:48:12 +0900

    ✨ [feature] schedule 컴포넌트 마운트시 schedule DB 조회 요청 로직

---
+++ src/services/fetchGetSchedules.js
@@ -0,0 +1,29 @@
+import generateApiUri from '../utils/generateURI';
+
+const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
+
+const fetchUserSchedules = async (user) => {
+  const BASE_URI = generateApiUri(SERVER_URI, `users/${user.userId}/diaries/schedules`);
+
+  try {
+    const response = await fetch(BASE_URI, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch user schedules');
+    }
+
+    const responseJson = await response.json();
+
+    return responseJson;
+  } catch (error) {
+    console.error('Error fetching user schedules:', error.message);
+  }
+};
+
+export default fetchUserSchedules;
