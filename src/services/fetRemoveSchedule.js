906abbb6492275a3672c23fd67985529ca9a5337
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-17 16:48:58 +0900

    ✨ [feature] schedule 삭제시 DB 삭제 요청 로직

---
+++ src/services/fetRemoveSchedule.js
@@ -0,0 +1,30 @@
+import generateApiUri from '../utils/generateURI';
+
+const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
+
+const fetchRemoveSchedule = async (scheduleObject, memberUser) => {
+  const BASE_URI = generateApiUri(SERVER_URI, `users/${memberUser.userId}/diaries/${scheduleObject.selectedDate}/schedule`);
+
+  try {
+    const response = await fetch(BASE_URI, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${memberUser.token}`,
+      },
+      body: JSON.stringify({ scheduleId: scheduleObject.scheduleId }),
+    });
+
+    if (!response.ok) {
+      console.error('Failed to delete plan from database');
+    }
+
+    const responseJson = await response.json();
+
+    return responseJson;
+  } catch (error) {
+    console.error('Error deleting plan from database:', error);
+  }
+};
+
+export default fetchRemoveSchedule;
