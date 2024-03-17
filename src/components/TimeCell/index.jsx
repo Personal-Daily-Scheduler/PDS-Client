f2eb18bb6dc3a321815ba323a7b25104e153d5c1
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-17 17:44:25 +0900

    🎨 [style] 시간셀 드래그시 셀 병합 스타일 추가

--- src/components/TimeCell/index.jsx
+++ src/components/TimeCell/index.jsx
@@ -1,48 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import darkenColor from '../../utils/darkenColor';
+
 function TimeCell({
   id, onDragEnd, isDragging, onMouseDown, onMouseEnter, schedule, hour,
 }) {
-  return hour === 0 || hour ? (
-    <HourCell>{hour}</HourCell>
-  ) : (
-    <CellWrapper
-      onMouseDown={onMouseDown}
-      onMouseUp={() => onDragEnd(id)}
-      onMouseEnter={() => onMouseEnter(id)}
-      isDragging={isDragging}
-      schedule={schedule}
-    />
+  const handleMouseup = (e) => {
+    onDragEnd(id, e);
+  };
+
+  return (
+    hour === 0 || hour ? (
+      <HoursCell>{hour}</HoursCell>
+    ) : (
+      <TimeCellWrapper
+        onMouseDown={onMouseDown}
+        onMouseUp={handleMouseup}
+        onMouseEnter={() => onMouseEnter(id)}
+        isDragging={isDragging}
+        schedule={schedule}
+      >
+        {(schedule.startTime === id.time) && schedule.title}
+      </TimeCellWrapper>
+    )
   );
 }
 
-const HourCell = styled.div`
+const HoursCell = styled.div`
   width: 35px;
   height: 22px;
   color: black;
   background-color: #55ff00;
   border: black 1px solid;
   margin: 0;
-  
+
   &:hover {
     background-color: #2e8304
   }
 `;
 
-const CellWrapper = styled.div`
+const TimeCellWrapper = styled.div`
   width: 35px;
   height: 22px;
   margin: 0;
-  border: ${({ isDragging }) => (isDragging ? 'white' : '#d9d9d9')} 1px solid;
-  background-color: ${(props) => (
-    props.isDragging ? '#ffcc00' : props.schedule ? props.schedule.colorCode : '#10f2aa'
+  border: 1px solid ${({ isDragging, schedule }) => (
+    isDragging ? darkenColor('#ffcc00', 10) : schedule ? darkenColor(schedule.colorCode, 10) : '#d9d9d9'
   )};
+  background-color: ${({ isDragging, schedule }) => (
+    isDragging ? '#ffcc00' : schedule ? schedule.colorCode : '#10f2aa'
+  )};
+  overflow: visible;
 
   &:hover {
-    background-color: ${(props) => (
-    props.isDragging ? '#928f8f' : props.schedule ? 'red' : '#fffffff1'
-  )}
+    background-color: ${({ isDragging, schedule }) => (isDragging ? '#928f8f' : schedule ? 'red' : '#fffffff1')}
   };
 `;
 
