f41c48e43fbb37fa958b3db5d3f48e41ab60f1e2
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-17 18:12:16 +0900

    ✨ [style] 모달 radius, 바깥 배경어둡기 설정 추가 & 화면 밖 렌더 버그 수정

--- src/shared/Modal/index.jsx
+++ src/shared/Modal/index.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import closeButtonHover from '../../assets/close_button_hover.png';
 import closeButtonDefault from '../../assets/close_button.png';
 
-function Modal({ onClose, children, style }) {
+function Modal({
+  onClose, children, style, darkBackground, borderRadius,
+}) {
+  const modalRef = useRef(null);
+  const [position, setPosition] = useState(style);
+
+  useEffect(() => {
+    const modalHeight = modalRef.current.offsetHeight;
+    const viewportHeight = window.innerHeight;
+
+    if (position.top + modalHeight > viewportHeight) {
+      const newTop = Math.max(viewportHeight - modalHeight - 20, 0);
+
+      setPosition({ ...style, top: newTop });
+    }
+  }, [onClose, position.top]);
+
   return (
-    <ModalOverlay>
-      <ModalContent style={style}>
+    <ModalOverlay darkBackground={darkBackground}>
+      <ModalContent ref={modalRef} style={position} borderRadius={borderRadius}>
         {children}
         <CloseButton onClick={onClose}></CloseButton>
       </ModalContent>
@@ -21,18 +37,20 @@ const ModalOverlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: ${(props) => (props.darkBackground ? 'rgba(0, 0, 0, 0.5)' : 'transparent')};
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index:10;
 `;
 
 const ModalContent = styled.div`
   background-color: #fff;
   padding: 20px;
-  border-radius: 5px;
+  border-radius: ${(props) => (props.borderRadius || '5px')};
   position: absolute;
   ${({ style }) => style && `left: ${style.left}px; top: ${style.top}px;`}
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
 const CloseButton = styled.button`
