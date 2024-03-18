8f10f0fe456eb8d6097ffad86d7ca056e9add74f
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-19 03:23:11 +0900

    💄 [design] 시간셀 기본색, 드래그 색,기본 이벤트색 변경 & 둥근모서리 효과

--- src/components/TimeCell/index.jsx
+++ src/components/TimeCell/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import darkenColor from '../../utils/darkenColor';
+import changeColor from '../../utils/changeColor';
+import formatHour from '../../utils/formatHour';
 
 function TimeCell({
   id, onDragEnd, isDragging, onMouseDown, onMouseEnter, schedule, hour,
@@ -12,7 +13,7 @@ function TimeCell({
 
   return (
     hour === 0 || hour ? (
-      <HoursCell>{hour}</HoursCell>
+      <HoursCell hour={hour}>{formatHour(hour)}</HoursCell>
     ) : (
       <TimeCellWrapper
         onMouseDown={onMouseDown}
@@ -28,15 +29,19 @@ function TimeCell({
 }
 
 const HoursCell = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 35px;
   height: 22px;
+  font-size: 12px;
   color: black;
-  background-color: #55ff00;
-  border: black 1px solid;
+  background-color: ${({ hour }) => (parseInt(hour, 10) < 12 ? '#fad3d3' : '#d8e0f9')};
+  border: #ebecef 1px solid;
   margin: 0;
 
   &:hover {
-    background-color: #2e8304
+    background-color: ${({ hour }) => (parseInt(hour, 10) < 12 ? changeColor('#fad3d3', 5) : changeColor('#d8e0f9', 5))};
   }
 `;
 
@@ -44,17 +49,19 @@ const TimeCellWrapper = styled.div`
   width: 35px;
   height: 22px;
   margin: 0;
-  border: 1px solid ${({ isDragging, schedule }) => (
-    isDragging ? darkenColor('#ffcc00', 10) : schedule ? darkenColor(schedule.colorCode, 10) : '#d9d9d9'
+  border: 0.5px solid ${({ isDragging, schedule }) => (
+    isDragging ? schedule ? changeColor(schedule.colorCode, 15) : changeColor('#f0f1f4', 10) : schedule ? changeColor(schedule.colorCode, 10) : changeColor('#f0f1f4', 10)
   )};
   background-color: ${({ isDragging, schedule }) => (
-    isDragging ? '#ffcc00' : schedule ? schedule.colorCode : '#10f2aa'
+    isDragging ? schedule ? changeColor(schedule.colorCode, 10) : '#f0f1f4' : schedule ? schedule.colorCode : '#ffffff'
   )};
   overflow: visible;
 
   &:hover {
-    background-color: ${({ isDragging, schedule }) => (isDragging ? '#928f8f' : schedule ? 'red' : '#fffffff1')}
-  };
+    background-color: ${({ isDragging, schedule }) => (
+    isDragging ? schedule ? changeColor(schedule.colorCode, 10) : '#928f8f' : schedule ? changeColor(schedule.colorCode, 10, true) : '#f0f1f4'
+  )};
+  }
 `;
 
 export default TimeCell;
