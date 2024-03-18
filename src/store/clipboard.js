e67b2e0712d2040ddbe5c0b79bbca13e87c0cc0e
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-19 02:17:39 +0900

    ✨ [feature] clipboard zustand 전역상태 설계

---
+++ src/store/clipboard.js
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+const clipboardStore = (set) => ({
+  copiedSchedule: null,
+  isCopied: false,
+  setCopiedSchedule: (copiedSchedule) => set({ copiedSchedule, isCopied: true }),
+  clearClipboard: () => set({ copiedSchedule: null, isCopied: false }),
+});
+
+const useClipboardStore = create(devtools(clipboardStore));
+
+export default useClipboardStore;
