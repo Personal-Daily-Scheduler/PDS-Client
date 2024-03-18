6cf30e212add7529f98f26d909bf7dab237d3395
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-19 02:21:57 +0900

    ✨ [feature] schedule 수정시 업데이트된 timeMap 반환 유틸함수

---
+++ src/utils/updateTimeMap.js
@@ -0,0 +1,37 @@
+import getTimeRange from './getTimeRange';
+
+const updateTimeMap = (timeMap, scheduleObject) => {
+  const targetTimeMap = new Map(timeMap);
+
+  const { scheduleId, startTime, endTime } = scheduleObject;
+
+  for (const [key, value] of timeMap) {
+    const { schedule } = value;
+
+    if (schedule !== '' && schedule.scheduleId === scheduleId) {
+      const oldTimeRange = getTimeRange(schedule.startTime, schedule.endTime);
+
+      oldTimeRange.forEach((scheduleTime) => {
+        targetTimeMap.set(scheduleTime, {
+          ...timeMap.get(scheduleTime),
+          schedule: '',
+        });
+      });
+
+      break;
+    }
+  }
+
+  const newTimeRange = getTimeRange(startTime, endTime);
+
+  newTimeRange.forEach((time) => {
+    targetTimeMap.set(time, {
+      index: timeMap.get(time).index,
+      schedule: scheduleObject,
+    });
+  });
+
+  return targetTimeMap;
+};
+
+export default updateTimeMap;
