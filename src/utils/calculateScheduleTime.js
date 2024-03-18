80699bfff51e97db00c3c20dd931c51d52bd430c
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-19 02:48:57 +0900

    ✨ [feature] 붙여넣기 시간에 맞춰 복사한 이벤트로부터 새 이벤트 반환 함수

---
+++ src/utils/calculateScheduleTime.js
@@ -0,0 +1,28 @@
+import { v4 as uuidV4 } from 'uuid';
+
+import differenceTime from './differenceTime';
+import timeToMinutes from './timeToMinutes';
+import minutesToTime from './minutesToTime';
+
+const calculatePasteSchedule = (pasteStartTime, targetEvent) => {
+  const { startTime, endTime } = targetEvent;
+
+  const differenceMinutes = differenceTime(startTime, endTime);
+  const pasteStartMinutes = timeToMinutes(pasteStartTime);
+
+  const pasteTargetTime = {
+    startTime: pasteStartTime,
+    endTime: minutesToTime(pasteStartMinutes + differenceMinutes),
+  };
+
+  const newScheduleId = uuidV4();
+
+  const newSchedule = {
+    ...targetEvent,
+    scheduleId: newScheduleId,
+    ...pasteTargetTime,
+  };
+  return newSchedule;
+};
+
+export default calculatePasteSchedule;
