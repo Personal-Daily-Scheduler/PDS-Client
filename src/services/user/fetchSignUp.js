7de048fdc797e649f33ed9bdaf3e0f601f754a73
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-21 06:39:30 +0900

    🎨 [style] user 관련 Fetch함수 디렉터리 변경

--- src/services/user/fetchSignUp.js
+++ src/services/user/fetchSignUp.js
@@ -1,27 +1,24 @@
-import generateApiUri from '../utils/generateURI';
+import generateApiUri from '../../utils/generateURI';
 
 const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
 
-const fetchLogin = async (userId, password) => {
-  const BASE_URI = generateApiUri(SERVER_URI, 'auth/login');
+const API_URI = generateApiUri(SERVER_URI, 'auth/signup');
 
+const fetchSignUp = async (username, email, password) => {
   try {
-    const response = await fetch(BASE_URI, {
+    const response = await fetch(API_URI, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        userId,
+        username,
+        email,
         password,
       }),
     });
 
     const responseJson = await response.json();
-
-    if (!responseJson.result) {
-      return { result: false, message: responseJson.message };
-    }
 
     return responseJson;
   } catch (error) {
@@ -29,4 +26,4 @@ const fetchLogin = async (userId, password) => {
   }
 };
 
-export default fetchLogin;
+export default fetchSignUp;
