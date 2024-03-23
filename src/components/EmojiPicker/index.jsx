7dab1ac56412df4135181430b2207d1a430780ad
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-24 01:38:27 +0900

    ✨ [feature] 이모지 Picker 컴포넌트 분리

---
+++ src/components/EmojiPicker/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
+
+function EmojiPicker({ onEmojiSelect, showEmojiPicker }) {
+  return showEmojiPicker && (
+    <EmojiPickerContainer>
+      <Picker data={data} onEmojiSelect={onEmojiSelect} />
+    </EmojiPickerContainer>
+  );
+}
+
+const EmojiPickerContainer = styled.div`
+  position: absolute;
+  top: 40px;
+  right: 0;
+  z-index: 1;
+`;
+
+export default EmojiPicker;
