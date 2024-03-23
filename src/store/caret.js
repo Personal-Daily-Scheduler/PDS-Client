0098042d6b90d3d75a680b4a2aa698f3d926f13b
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-24 00:40:27 +0900

    ✨ [feature] textEditor Caret위치 전역상태 추가

---
+++ src/store/caret.js
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+const caretStore = (set) => ({
+  cursorPosition: null,
+  setCursorPosition: (position) => set({ cursorPosition: position }),
+});
+
+const useCursorPositionStore = create(devtools(caretStore));
+
+export default useCursorPositionStore;
