61aaba6fec7b6c4a4e52d56d7994424927cd8fb1
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-04-11 21:54:39 +0900

    [style] 테스트용 콘솔 로그 삭제

--- src/services/user/fetchLogin.js
+++ src/services/user/fetchLogin.js
@@ -3,8 +3,10 @@ import generateApiUri from '../../utils/generateURI';
 const SERVER_URI = import.meta.env.VITE_BACKEND_BASE_URI;
 
 const fetchLogin = async (userId, password) => {
+  const BASE_URI = generateApiUri(SERVER_URI, 'auth/login');
+
   try {
-    const response = await fetch('https://api.pdsdiary.com/auth/login', {
+    const response = await fetch(BASE_URI, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
@@ -15,7 +17,6 @@ const fetchLogin = async (userId, password) => {
       }),
     });
 
-    console.log('서버로 부터 응답', response);
     const responseJson = await response.json();
 
     if (!responseJson.result) {
