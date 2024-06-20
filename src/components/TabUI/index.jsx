2d7ab71d1deb2c91f3843aa567ff6006e3b70746
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-06-20 16:41:16 +0900

    [feature] TabUI 컴포넌트 레이아웃 작성

---
+++ src/components/TabUI/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+import IconTextButton from "../../shared/IconButton";
+
+import HomeIcon from "../../assets/home_button_icon.png";
+import ScheduleIcon from "../../assets/schedule_icon.png";
+import TimeCellIcon from "../../assets/timecell_button_icon.png";
+import TextEditorIcon from "../../assets/texteditor_icon.png";
+import AddIcon from "../../assets/add_icon.png";
+import TodayIcon from "../../assets/today_icon_2.png";
+
+function TabUI({ onViewModeChange }) {
+  return (
+    <TabContainer>
+      <IconTextButton iconSrc={AddIcon} size="30px"></IconTextButton>
+      <IconTextButton iconSrc={HomeIcon} size="30px" onClick={(e) => onViewModeChange("all")}></IconTextButton>
+      <IconTextButton iconSrc={ScheduleIcon} size="30px" onClick={(e) => onViewModeChange("schedules")}></IconTextButton>
+      <IconTextButton iconSrc={TimeCellIcon} size="30px" onClick={(e) => onViewModeChange("plans")}></IconTextButton>
+      <IconTextButton iconSrc={TextEditorIcon} size="30px" onClick={(e) => onViewModeChange("editor")}></IconTextButton>
+      <IconTextButton iconSrc={TodayIcon} size="30px"></IconTextButton>
+    </TabContainer>
+  );
+}
+
+const TabContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 60px;
+  background-color: white;
+  border-top: 1px solid #d9d9d9;
+`;
+
+export default TabUI;
