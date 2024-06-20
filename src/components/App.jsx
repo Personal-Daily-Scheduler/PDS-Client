28677c99d24536e44becc457080d24a19d92bd8f
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-06-20 16:50:07 +0900

    [feature] 모바일 화면에서 TabUI 클릭시 화면변경

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 import Login from "./Login";
@@ -6,8 +7,31 @@ import Layout from "./Layout";
 import Plans from "./Plans";
 import Schedules from "./Schedules";
 import TextEditor from "./See";
+import TabUI from "./TabUI";
+
+import useMobileStore from "../store/useMobileStore";
 
 function App() {
+  const [viewMode, setViewMode] = useState("all");
+  const { isMobile, setIsMobile } = useMobileStore();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 748);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [setIsMobile]);
+
+  const handleViewModeChange = (mode) => {
+    setViewMode(mode);
+  };
+
   return (
     <>
       <GlobalStyle />
@@ -18,11 +42,14 @@ function App() {
             path="/users"
             exact
             element={(
-              <Wrapper>
-                <Plans className="horizontal" />
-                <Schedules className="horizontal" />
-                <TextEditor className="horizontal vertical" />
-              </Wrapper>
+              <>
+                <Wrapper>
+                  {(viewMode === "all" || viewMode === "plans") && <Plans />}
+                  {(viewMode === "all" || viewMode === "schedules") && <Schedules />}
+                  {(viewMode === "all" || viewMode === "editor") && <TextEditor />}
+                </Wrapper>
+                {isMobile && <TabUI onViewModeChange={handleViewModeChange} />}
+              </>
             )}
           />
         </Route>
@@ -41,52 +68,6 @@ const GlobalStyle = createGlobalStyle`
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  flex-wrap: nowrap;
-  transition: all 0.3s ease-in-out;
-
-  .horizontal {
-    flex-basis: calc(33.33% - 20px);
-    background-color: #f1f1f1;
-    margin: 0;
-    margin-right: 20px;
-    text-align: center;
-    transition: flex-basis 0.3s ease-in-out;
-
-    &:last-child {
-      margin-right: 0;
-    }
-
-    @media screen and (max-width: 900px) and (min-width: 651px) {
-      flex-basis: calc(50% - 10px);
-      margin-right: 10px;
-      margin-bottom: 10px;
-
-      &:last-child {
-        margin-right: 0;
-      }
-    }
-
-    @media screen and (max-width: 650px) {
-      flex-basis: 100%;
-      margin-right: 0;
-      margin-bottom: 20px;
-    }
-  }
-
-  .vertical {
-    @media screen and (max-width: 900px) {
-      flex-basis: 100%;
-      margin-right: 0;
-    }
-  }
-
-  @media screen and (max-width: 900px) {
-    flex-wrap: wrap;
-  }
-
-  @media screen and (max-width: 650px) {
-    flex-direction: column;
-  }
 `;
 
 export default App;
