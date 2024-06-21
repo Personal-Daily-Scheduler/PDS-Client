54851ba566a67f07194a62c940d3207d1660df32
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-06-21 16:13:48 +0900

    [feature] 시간 셀 반응형 적용

--- src/components/TimeCell/index.jsx
+++ src/components/TimeCell/index.jsx
@@ -3,17 +3,20 @@ import styled from "styled-components";
 
 import changeColor from "../../utils/changeColor";
 import formatHour from "../../utils/formatHour";
+import useMobileStore from "../../store/useMobileStore";
 
 function TimeCell({
-  id, onDragEnd, isDragging, onMouseDown, onMouseEnter, schedule, hour,
+  id, onDragEnd, isDragging, onMouseDown, onMouseEnter, schedule, hour, viewMode,
 }) {
+  const { isMobile } = useMobileStore();
+
   const handleMouseup = (e) => {
     onDragEnd(id, e);
   };
 
   return (
     hour === 0 || hour ? (
-      <HoursCell hour={hour}>{formatHour(hour)}</HoursCell>
+      <HoursCell hour={hour} isMobile={isMobile} viewMode={viewMode}>{formatHour(hour)}</HoursCell>
     ) : (
       <TimeCellWrapper
         onMouseDown={onMouseDown}
@@ -21,6 +24,8 @@ function TimeCell({
         onMouseEnter={() => onMouseEnter(id)}
         isDragging={isDragging}
         schedule={schedule}
+        viewMode={viewMode}
+        isMobile={isMobile}
       >
         {schedule.startTime === id.time && (
           <ScheduleTitle>{schedule.title}</ScheduleTitle>
@@ -34,13 +39,18 @@ const HoursCell = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 35px;
-  height: 22px;
+  width: ${({ viewMode, isMobile }) => (isMobile ? viewMode === "home" ? "35px" : "45px" : "35px")};
+  height: ${({ viewMode, isMobile }) => (isMobile ? viewMode === "home" ? "18px" : "30px" : "30px")};
   font-size: 12px;
   color: black;
   background-color: ${({ hour }) => (parseInt(hour, 10) < 12 ? "#fad3d3" : "#d8e0f9")};
   border: #ebecef 1px solid;
   margin: 0;
+
+  @media (max-width: 980px) and (min-width: 748px) {
+    width: 30px;
+    height: 22px;
+  }
 
   &:hover {
     background-color: ${({ hour }) => (parseInt(hour, 10) < 12 ? changeColor("#fad3d3", 5) : changeColor("#d8e0f9", 5))};
@@ -48,8 +58,8 @@ const HoursCell = styled.div`
 `;
 
 const TimeCellWrapper = styled.div`
-  width: 35px;
-  height: 22px;
+  width: ${({ viewMode, isMobile }) => (isMobile ? viewMode === "home" ? "23px" : "32px" : "35px")};
+  height: ${({ viewMode, isMobile }) => (isMobile ? viewMode === "home" ? "18px" : "30px" : "30px")};
   margin: 0;
   border: 0.5px solid ${({ isDragging, schedule }) => (
     isDragging ? schedule ? changeColor(schedule.colorCode, 15) : changeColor("#f0f1f4", 10) : schedule ? changeColor(schedule.colorCode, 10) : changeColor("#f0f1f4", 10)
@@ -63,6 +73,11 @@ const TimeCellWrapper = styled.div`
     background-color: ${({ isDragging, schedule }) => (
     isDragging ? schedule ? changeColor(schedule.colorCode, 10) : "#928f8f" : schedule ? changeColor(schedule.colorCode, 10, true) : "#f0f1f4"
   )};
+  }
+
+  @media (max-width: 980px) and (min-width: 748px) {
+    width: 36px;
+    height: 22px;
   }
 `;
 
